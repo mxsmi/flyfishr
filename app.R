e79fdd230d535b7df7b08acf9f4c1c8900ffc874
mxsmi <45386017+mxsmi@users.noreply.github.com>
2025-06-16 19:31:03 -0700

    updated project directory structure
    updated project directory structure

--- app.R
+++ app.R
@@ -1,60 +1,6 @@
-### StreamNotes App. A data dashboard for fly fishermen to view current water
-### levels for any body of water in the country and get an AI generated fishing
-### report.
+## Shiny app primary document
 
-## Load required libraries
-library(shiny)
-library(dataRetrieval)
-library(ggplot2)
-library(waiter)
-library(lubridate)
-library(dplyr)
-library(stringr)
-library(markdown)
-library(chatLLM)
-library(leaflet)
 library(devtools)
-
-## Set API key
-if (file.exists("github_models_api_key.env")) {
-  readRenviron("github_models_api_key.env")
-}
-
 devtools::load_all()
+flyfishrApp()
 
-ui <- function(request) {
-  fluidPage(
-
-    waiter::use_waiter(),
-    titlePanel("flyfishr App"),
-
-    inputControlsUI("controls"),
-
-    tabsetPanel(
-      tabPanel("Map", mapUI("map")),
-      tabPanel("Fly Fishing Report", fishingReportUI("report")),
-      tabPanel("Charts", chartsUI("charts"))
-    )
-  )
-}
-
-server <- function(input, output, session) {
-
-  search_data <- inputControlsServer("controls")
-  water_data <- waterDataServer(id = "data",
-                                sites = search_data$sites,
-                                selected_site = search_data$selected_site)
-  chartsServer("charts", water_data)
-  mapServer("map", water_data)
-  fishingReportServer("report", search_data$selected_site, water_data)
-
-  # Automatically bookmark every time an input changes
-  observe({
-    reactiveValuesToList(input)
-    session$doBookmark()
-  })
-  # Update the query string
-  onBookmarked(updateQueryString)
-}
-
-shinyApp(ui, server, enableBookmarking = "url")
